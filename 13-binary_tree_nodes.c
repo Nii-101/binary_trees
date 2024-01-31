84fe7f6eef53317dae6317d1fef0e2e3246a2652
Nii-101 <noelamptey101@gmail.com>
2024-01-31 03:18:42 +0000

    Documentation and coding style debugging

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
  * binary_tree_nodes - It returns number of nodes in a tree with children
- * 
+ *
  * @tree: A pointer to treee
- * 
+ *
  * Return: size_t nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
