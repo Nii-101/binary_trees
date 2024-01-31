8e6fd57a46157cd684164ee9272be4efb9cfcde8
Nii-101 <noelamptey101@gmail.com>
2024-01-31 03:03:48 +0000

    finished tak 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+* binary_tree_depth - gets the depth of a node from the root
+*
+* @tree: A pointer to node
+* 
+* Return: Returns an int
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t tree_depth = 0;
+
+	if (tree && tree->parent)
+	{
+		tree_depth = 1 + binary_tree_depth(tree->parent);
+	}
+	else
+	{
+		tree_depth = 0;
+	}
+	return (tree_depth);
+}
