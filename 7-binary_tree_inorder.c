ab58f73b6e2c08db02c529c10d3d2a9269472d3f
Nii-101 <noelamptey101@gmail.com>
2024-01-31 03:03:09 +0000

    Task 7 finished

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_inorder - outputs the values of tree using in-order
+*
+* @tree: A pointer to tree
+*
+* @func: A pointer to function
+* 
+* Return: void
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
