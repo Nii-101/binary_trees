33cbe7bc7853d3778f45e97509098d88a27c4806
Nii-101 <noelamptey101@gmail.com>
2024-01-31 03:03:32 +0000

    done task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - It computes the height of a binary tree
+*
+* @tree: A pointer to tree
+* 
+* Return: returns int
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			if (tree->left)
+			{
+				left_height = 1 + binary_tree_height(tree->left);
+			}
+			else
+			{
+				left_height = 0;
+			}
+
+			if (tree->right)
+			{
+				right_height = 1 + binary_tree_height(tree->right);
+			}
+			else
+			{
+				right_height = 0;
+			}
+
+		}
+
+		if (left_height > right_height)
+		{
+			return (left_height);
+		}
+		else
+		{
+			return (right_height);
+		}
+	}
+}
