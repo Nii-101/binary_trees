5e8c4f2f0d53a5e7ab57fe0a2e152707d79fae2b
Nii-101 <noelamptey101@gmail.com>
2024-01-31 03:02:21 +0000

    Finished task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+*
+* binary_tree_delete - deletes a binary tree
+*
+* @tree: The binary tree to be deleted
+*
+* Return: void
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
+}
